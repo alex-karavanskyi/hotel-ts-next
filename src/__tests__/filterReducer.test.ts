6fdfacc6dfe046aebb95f39e349a4e8dbb8a2214
alexfront2007 <alexmarr2007@gmail.com>
2023-11-06 09:00:36 +0200

    reducers test

---
+++ src/__tests__/filterReducer.test.ts
@@ -0,0 +1,20 @@
+import filterReducer from '../app/redux/features/filterSlice'
+
+describe('filterReducer', () => {
+  it('should return default state when passed an empty action', () => {
+    const result = filterReducer(undefined, { type: '' })
+    expect(result).toEqual({
+      all_products: [],
+      filtered_products: [],
+      filters: {
+        category: 'all',
+        max_price: 0,
+        min_price: 0,
+        price: 0,
+        text: '',
+      },
+      grid_view: true,
+      sort: 'price-lowest',
+    })
+  })
+})
---
+++ src/__tests__/modalReducer.test.ts
@@ -0,0 +1,26 @@
+import modalReducer, {
+  openModal,
+  closeModal,
+} from '../app/redux/features/modalSlice'
+
+describe('modalSlice', () => {
+  it('should handle openModal', () => {
+    const initialState = { isOpen: false }
+    const nextState = modalReducer(initialState, openModal())
+    expect(nextState.isOpen).toEqual(true)
+  })
+
+  it('should handle closeModal', () => {
+    const initialState = { isOpen: true }
+    const nextState = modalReducer(initialState, closeModal())
+    expect(nextState.isOpen).toEqual(false)
+  })
+
+  it('should return the initial state if no action is provided', () => {
+    const initialState = { isOpen: false }
+    const nextState = modalReducer(initialState, {
+      type: undefined,
+    })
+    expect(nextState).toEqual(initialState)
+  })
+})
