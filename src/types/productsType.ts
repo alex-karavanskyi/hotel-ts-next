9136e27c95d216a0dbaaf01b00b18e69e4b045d9
alexfront2007 <alexmarr2007@gmail.com>
2024-07-05 12:22:18 +0300

    refactor productsType

--- src/types/productsType.ts
+++ src/types/productsType.ts
@@ -3,43 +3,9 @@ type Products = {
   name: string
   price: number
   image: string
-  colors: string[]
-  company: string
-  shipping: boolean
   description: string
   category: string
-  images?: [
-    {
-      id: string
-      width: number
-      height: number
-      url: string
-      filename: string
-      size: number
-      type: string
-      thumbnails: {
-        small: {
-          url: string
-          width: number
-          height: number
-        }
-        large: {
-          url: string
-          width: number
-          height: number
-        }
-        full: {
-          url: string
-          width: number
-          height: number
-        }
-      }
-    }
-  ]
-  stock?: number
-  featured?: boolean
-  reviews?: number
-  stars?: number
+  images: []
 }
 
 export default Products
