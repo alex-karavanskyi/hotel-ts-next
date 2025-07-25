6553a9fd30d86dbdb3f42d73da6c6341e61adb8a
Alex <alexmarr2007@gmail.com>
2025-07-25 11:40:52 +0300

    fix: update social links and icons in footer

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -3,6 +3,6 @@ import Sidebar from './Sidebar'
 import Navbar from './Navbar'
 import Error from './Error'
 import Loading from './Loading'
-import ClientOnly from '../shared/utils/clientOnly'
+import ClientOnly from './ClientOnly'
 
 export { Footer, Sidebar, Navbar, Error, Loading, ClientOnly }
--- src/shared/ui/SocialLinks.tsx
+++ src/shared/ui/SocialLinks.tsx
@@ -1,5 +1,8 @@
 'use client'
 import styled from 'styled-components'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaTelegram } from 'react-icons/fa'
 
 const SocialLinks = () => {
   return (
@@ -9,20 +12,20 @@ const SocialLinks = () => {
           href='https://github.com/alex-karavanskyi'
           className='social__links-icon'
         >
-          <i className='fab fa-github'></i>
+          <FaGithub />
         </a>
       </li>
       <li>
         <a
-          href='www.linkedin.com/in/oleksandr-karavanskyi-710770309'
+          href='https://linkedin.com/in/oleksandr-karavanskyi-710770309'
           className='social__links-icon'
         >
-          <i className='fab fa-linkedin'></i>
+          <FaLinkedin />
         </a>
       </li>
       <li>
-        <a href='https://www.instagram.com' className='social__links-icon'>
-          <i className='fab fa-instagram'></i>
+        <a href='https://t.me/alex_karavanskyi' className='social__links-icon'>
+          <FaTelegram />
         </a>
       </li>
     </Container>
